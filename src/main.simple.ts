9929a6707af11737fff3d3b3c8416f6e5252b234
majlando <rene@majland.de>
2025-06-19 08:53:36 +0200

    Remove unused .simple.* files and package.simple.json

--- src/main.simple.ts
+++
@@ -1,7 +0,0 @@
-import './style.simple.css'
-import { FlagsApp } from './components/FlagsApp.simple'
-
-// Simple initialization
-document.addEventListener('DOMContentLoaded', () => {
-  new FlagsApp();
-});
